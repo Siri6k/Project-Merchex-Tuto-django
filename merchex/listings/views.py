992bdeeaaf4a5fe2b4a2da9f2790e9f0de84c91c
Sirisk <adamchrisk@gmail.com>
2022-10-22 23:01:17 +0200

    Utilisation des gabarits pour les templates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Band
 
-# Create your views here.
+def hello(request):
+    bands = Band.objects.all()
+    return render(request,
+                  'listings/hello.html',
+                  {'bands': bands})
+def about(request):
+    return HttpResponse("""<h1>hello Django</h1>
+<p>je m'appelle Adam chris Kayungura</p>
+<p>Mon num +243 994126699 <br> email: adamchrisk@gmail.com</p>
+""")
