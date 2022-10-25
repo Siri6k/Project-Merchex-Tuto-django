11b1fcde793379a515f5b1df6835a30f62da742c
Sirisk <adamchrisk@gmail.com>
2022-10-25 23:47:10 +0200

    Creation d'une vue en liste et une vue detailée

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from .models import Band
+from .models import Band, Listing
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
     return render(request,
-                  'listings/hello.html',
+                  'listings/bands.html',
                   {'bands': bands})
+def band_detail(request, band_id):
+    # notez le parametre band_id supplementaire permet d'identifier chaque band
+    band = Band.objects.get(id=band_id)
+    return render(request,
+                  'listings/band_detail.html',
+                  {'band': band})
+def listing_list(request):
+    list = Listing.objects.all()
+    return render(request,
+                  'listings/listing_list.html',
+                  {'items': list})
+def listing_detail(request, list_id):
+    # notez le parametre band_id supplementaire permet d'identifier chaque band
+    list = Listing.objects.get(id=list_id)
+    return render(request,
+                  'listings/listing_detail.html',
+                  {'item': list})
 def about(request):
     return HttpResponse("""<h1>hello Django</h1>
 <p>je m'appelle Adam chris Kayungura</p>
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -4,6 +4,9 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
+    path('bands/', views.band_list, name='band-list'),
+    path('bands/<int:band_id>/', views.band_detail, name="band-detail"),
+    path('listings/', views.listing_list, name="listing-list"),
+    path('listings/<int:list_id>/', views.listing_detail, name="listing-detail"),
     path('about-us/', views.about),
 ]
