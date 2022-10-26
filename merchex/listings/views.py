ff7a8150126bc81fae70e855c2ec7be71854a9ab
Sirisk <adamchrisk@gmail.com>
2022-10-26 19:31:41 +0200

    Creez des groupes et des annonces avec ModelForm

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Band, Listing
+from .forms import ContactUsForm
+from django.core.mail import send_mail
+from listings.forms import BandForm, ListingForm
 
 def band_list(request):
     bands = Band.objects.all()
@@ -23,8 +26,47 @@ def listing_detail(request, list_id):
     return render(request,
                   'listings/listing_detail.html',
                   {'item': list})
+def band_create(request):
+    choice = 'band'
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm()
+    return render(request,
+                  'listings/create.html',
+                  {'form': form,
+                   'choice':choice})
+def listing_create(request):
+    choice='listing'
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListingForm()
+    return render(request,
+                  'listings/create.html',
+                  {'form': form,
+                   'choice':choice})
+def contact(request):
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                subject=f'Message de {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact-us Form',
+            message=form.cleaned_data['message'],
+            from_email=form.cleaned_data['email'],
+            recipient_list=['admin@merchex.xyz'])
+            return redirect('band-list')
+    else:
+        form = ContactUsForm()
+    return render(request,
+                  'listings/contact.html',
+                  {'form': form})
 def about(request):
-    return HttpResponse("""<h1>hello Django</h1>
-<p>je m'appelle Adam chris Kayungura</p>
-<p>Mon num +243 994126699 <br> email: adamchrisk@gmail.com</p>
-""")
+    return render(request,
+                  'listings/about.html')
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -4,9 +4,15 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # afficahage en liste et en detail des groupes et des annonces
     path('bands/', views.band_list, name='band-list'),
     path('bands/<int:band_id>/', views.band_detail, name="band-detail"),
     path('listings/', views.listing_list, name="listing-list"),
     path('listings/<int:list_id>/', views.listing_detail, name="listing-detail"),
-    path('about-us/', views.about),
+    # creation d'une bande et des annonces
+    path('bands/add/', views.band_create, name="band-create"),
+    path('listings/add/', views.listing_create, name="listing-create"),
+    # contactez nous
+    path('contact/', views.contact, name="contact-us"),
+    path('about/', views.about, name="about-us"),
 ]
